f508162958f35063278d0293deb8b354c9f65840
Anton Titov <wolfy.jd@gmail.com>
2016-01-15 22:39:46 +0300

    Update ActorInterface.php

--- source/Security/ActorInterface.php
+++ source/Security/ActorInterface.php
@@ -14,7 +14,7 @@ namespace Spiral\Security;
 interface ActorInterface
 {
     /**
-     * Method must return list of roles associated with current operation is a form of array.
+     * Method must return list of roles associated with current actor is a form of array.
      *
      * @return array
      */
